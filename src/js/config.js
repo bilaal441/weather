74d8d8ab4d2eee7462687382979e14750406a453
bilal <bilaal441@hotmail.com>
2021-07-25 15:30:36 +0100

    https updated

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const OPENWEATHER_API_URL =
-  'http://api.openweathermap.org/data/2.5/onecall';
+  'https://api.openweathermap.org/data/2.5/onecall';
 export const CENT_NUMBER_DAYS = 8;
 export const OPENWEATHER_API_KEY = '8aced336c84ab76e9583a445e35471ea';
 export const TIMEOUT_SEC = 10;
